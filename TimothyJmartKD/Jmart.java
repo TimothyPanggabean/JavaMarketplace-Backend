8e55e36954dcf965b1c98f705b7e60ae7406a76c
TimothyPanggabean <timothy.christian91@ui.ac.id>
2021-09-11 11:17:56 +0700

    modul1 Timothy

--- TimothyJmartKD/Jmart.java
+++ TimothyJmartKD/Jmart.java
@@ -13,35 +13,67 @@ public class Jmart
         
     }
 
-    public int getPromo()
+    public static int getPromo()
     {
        return 0;
     }
     
-    public String getCustomer()
+    public static String getCustomer()
     {
         return "oop";
     }
     
-    public float getDiscountPercentage(int before, int after)
+    public static float getDiscountPercentage(int before, int after)
     {
-        float persenDiskon;
+        float DiscountPercentage;
         
         if (before>after)
         {
-            persenDiskon = before - after;
-            persenDiskon = (persenDiskon * 100) / before;
+            DiscountPercentage = before - after;
+            DiscountPercentage = (DiscountPercentage * 100) / before;
         }
         else
         {
-            persenDiskon = 0;
+            DiscountPercentage = 0;
         }
-        return persenDiskon;
+        return DiscountPercentage;
     }
      
-    public float DiscountedPrice(int price, float discountPercentage)
+    public static int getDiscountedPrice(int price, float discountPercentage)
     {
-       return 0;
+        int DiscountedPrice;
+      if(discountPercentage > 100.0f) 
+        {
+            return 100;
+        }
+        else 
+        {
+            return (int)(price - (price * discountPercentage / 100));
+        }
+    
     }
     
+     public static int getOriginalPrice(int discountedPrice, float discountPercentage)
+    {
+       return (int)(discountedPrice / (discountPercentage / 100));
+    }
+    
+     public static float getCommissionMultiplier()
+    {
+       return 0.05f;
+    }
+    
+     public static int getAdjustedPrice(int price)
+    {
+       int sementara, AdjustedPrice;
+       
+       sementara = (int)(price * getCommissionMultiplier());
+       AdjustedPrice = price + sementara;
+       return AdjustedPrice;
+    }
+    
+     public static int getAdminFee(int price)
+    {
+       return (int)(price * getCommissionMultiplier());
+    }
 }
