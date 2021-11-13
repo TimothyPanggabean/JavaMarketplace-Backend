04c75271413228dacb2af11b3d587f679050bd04
TimothyPanggabean <timothy.christian91@ui.ac.id>
2021-11-13 10:53:41 +0700

    CS modul7 Timothy

---
+++ TimothyJmartKD/JsonTable.java
@@ -0,0 +1,43 @@
+package TimothyJmartKD;
+
+import java.io.*; 
+import java.lang.reflect.Array; 
+import java.util.Collections; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector; 
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+
+public class JsonTable<T> extends Vector 
+{
+	public final String filepath;
+	private static final Gson gson = new Gson();
+	
+	@SuppressWarnings("unchecked")
+	public JsonTable(Class<T> clazz, String filepath) throws IOException
+	{
+		this.filepath = filepath;
+		Class<T[]> arrayType = (Class<T[]>) Array.newInstance(clazz, 0).getClass();
+		T[] loaded = readJson(arrayType, filepath);
+		for(T element: loaded) this.add(element);
+	}
+	
+	public static<T> T readJson(Class<T> clazz, String filepath) throws FileNotFoundException
+	{
+		final JsonReader reader = new JsonReader(new FileReader(filepath));
+		return gson.fromJson(reader, clazz);
+	}
+	
+	public void writeJson() throws IOException
+	{
+		final FileWriter writer = new FileWriter(this.filepath);
+		gson.toJson(gson, writer);
+	}
+	
+	public static void writeJson(Object object, String filepath) throws IOException
+	{
+		final FileWriter writer = new FileWriter(filepath);
+		gson.toJson(object, writer);
+	}
+}
